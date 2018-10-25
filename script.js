3e1255eea41667b583b2d6f407166d2ec600644f
gabriel <gabe.wilk@gmail.com>
2018-10-25 11:21:13 -0400

    flipp

--- script.js
+++ script.js
@@ -1,2 +1,35 @@
 "use strict";
 
+$(document).ready(()=>{
+    let card = null;
+    // function flip() {
+    //     $('.card').toggleClass('flipped');
+    // }
+    
+     $(document).on("click", ".card", (event) => {
+            console.log("test");
+
+            $('.card').toggleClass('flipped');
+
+        //     console.log("test");
+        //    card = $(event.target);
+        //    card.css("width", 0);
+        //    $(".front").css("display","flex");
+        //    $(".front").css("width", 100);
+
+               // .css("display",flex)
+            // $(event.target).
+            // $(".card")
+            // .addClass("byeBack").css("width", 0)
+            // .addClass("front").css("width", 100);
+            //                  .removeClass("back").removeClass("card");
+            
+                            
+        
+            });
+
+});
+            
+            
+
+    

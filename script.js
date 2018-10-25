8df02c249e7873da2ec091f9c513d3c7886d1d2a
christina <cmmiller.406@gmail.com>
2018-10-25 09:51:17 -0400

    added styling and timer

--- script.js
+++ script.js
@@ -1,2 +1,16 @@
 "use strict";
 
+$(document).ready(() => {
+    $(document).on("click", ".start-btn", (event) => {
+
+        setInterval(updateDisplay, 1000);
+        function updateDisplay() {
+            var value = parseInt($("#timer").find(".value").text(), 10);
+            value++;
+            $("#timer").find(".value").text(value);
+        }
+
+
+
+    })
+})
